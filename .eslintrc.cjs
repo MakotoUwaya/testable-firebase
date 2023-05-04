4d3ebccd2fc9fdad38f40d8d31712e089f46712b
oichan <makoto.uwaya@gmail.com>
2023-05-04 11:53:54 +0900

    🔧 Fix ESLint settings for React

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,13 +5,16 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
         "plugin:@typescript-eslint/recommended"
     ],
     "overrides": [
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
@@ -20,5 +23,7 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off"
     }
 }
