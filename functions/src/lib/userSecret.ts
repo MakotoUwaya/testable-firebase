516b61cdccc2a628ef057ee1101a13cbcd335619
oichan <makoto.uwaya@gmail.com>
2023-09-18 18:32:35 +0900

    Add FCM Functions

---
+++ functions/src/lib/userSecret.ts
@@ -0,0 +1,10 @@
+import { db, FieldPath, CollectionReference, Query } from "./firebase";
+import { UserSecretDocumentData } from "../types/userSecret";
+
+export const userSecretsRef = db.collection(
+  "userSecrets"
+) as CollectionReference<UserSecretDocumentData>;
+export const userSecretsWithoutSenderQuery = (
+  senderId: string
+): Query<UserSecretDocumentData> =>
+  userSecretsRef.where(FieldPath.documentId(), "!=", senderId);
---
+++ functions/src/types/message.ts
@@ -0,0 +1,10 @@
+import { Timestamp } from "../lib/firebase";
+import { WithId } from "./firebase";
+
+export type MessageDocumentData = {
+  createdAt: Timestamp;
+  content: string;
+  imagePath: string | null;
+  senderId: string;
+};
+export type Message = WithId<MessageDocumentData>;
